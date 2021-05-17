85932a3c22283732d8b4469daa7e0a65bdbd4b47
zeronoshigure <76189173+zeronoshigure@users.noreply.github.com>
2021-05-18 08:42:20 +0900

    訂正

--- python/L7/tuple.py
+++ python/L7/tuple.py
@@ -11,9 +11,26 @@ print(tp[2])#not tp(2)
 #
 mydict = {"apple":100, "orange":90, "banana":120}
 print(mydict)
+mydict["apple"]=150
+onedict=mydict.copy()
+print(onedict)
+del mydict["banana"]
+twodict=mydict.copy()
+print(twodict)
 '''
-() : parentheses
-[] : square brackets
-{} : braces
+() : parentheses     tuple
+[] : square brackets list
+{} : braces          dict
 さらにネストさせたい場合は {[({[()]})]} 
 '''
+#dictの高度な使い方
+
+for i in mydict.keys():
+    print(i,end="\t")
+for j in mydict.values():
+    print(j,end="\t")
+for k in mydict.items():
+    print(k,end="\t")
+
+mydict.update(onedict)#辞書の結合
+print(mydict)
