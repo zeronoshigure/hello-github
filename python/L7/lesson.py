03a876b0c200013348da71e61028656d72bac51f
zeronoshigure <76189173+zeronoshigure@users.noreply.github.com>
2021-07-13 09:48:09 +0900

    lesson
    lessonfile

---
+++ python/L7/lesson.py
@@ -0,0 +1,31 @@
+#lesson1#############
+a=3
+b=3.14
+c="Hello World!"
+
+print(a,b,c)
+
+###################
+
+#lesson2############
+d=input("Plz enter some  words.")
+
+print(d)
+
+##################
+
+#lesson3############
+e=input("Plz enter some numbers.")
+if int(e)>=10:
+    print("You entered a number of 10 or more.")
+elif int(e)<=10:
+    print("You entered a number below 10.")
+else:
+    print("Plz enter some numbers.")
+#################
+
+#lesson4###########
+for i in range(100):
+    print(i)
+
+################
